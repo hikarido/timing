66ef7975c9e0b718782c0e2fef92cabef61c0904
Konstantin Ananev <ananevkv@yoomoney.ru>
2022-08-08 22:59:11 +0300

    record was added

---
+++ src/main/java/me/ananev/ReportRecord.java
@@ -0,0 +1,9 @@
+package me.ananev;
+
+/**
+ * TODO javadoc
+ *
+ * @author Ananev Konstantin (ananevkv@yoomoney.ru)
+ * @since 08.08.2022
+ */
+public record ReportRecord(Integer line, java.time.LocalTime time, String taskName) { }
